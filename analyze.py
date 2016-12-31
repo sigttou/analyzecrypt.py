8bbf5bef3d6c45a22899ac1d1b0e9aed0a985a8c
David Bidner <david.bidner@student.tugraz.at>
2016-12-31 12:47:01 +0100

    ADD: simple notification if given function is called

--- analyze.py
+++ analyze.py
@@ -5,6 +5,13 @@ from pwn import log
 
 MODULES = ["libc.so", "libcrypto.so"]
 LOOKFOR = "AES_cbc_encrypt"
+SCRIPT = """
+    Interceptor.attach(ptr("{}"), {{
+        onEnter: function(args) {{
+            send(args[0].toString(16));
+        }}
+    }});
+"""
 
 
 def on_message(message, data):
@@ -29,11 +36,17 @@ def main(target):
     for x in modules:
         functions += x.enumerate_exports()
     log.info("Found {} functions".format(len(functions)))
+
     result = functions[[x.name for x in functions].index(LOOKFOR)]
     log.info("Found {} in {} @ {}".format(result.name,
                                           result.module.name,
                                           hex(result.absolute_address)
                                           ))
+
+    script = session.create_script(SCRIPT.format(result.absolute_address))
+    script.on('message', on_message)
+    script.load()
+    sys.stdin.read()
 
 
 if __name__ == '__main__':
